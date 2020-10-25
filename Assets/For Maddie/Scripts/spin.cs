72762d793a1ab8747a277906c5617fe83bdf1bb9
starfaerie <54830622+starfaerie@users.noreply.github.com>
2020-10-25 15:15:06 -0600

    Initial commit

---
+++ Assets/For Maddie/Scripts/spin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class spin : MonoBehaviour
+{
+    [Range(0.2f, 10f)]
+    public float rotationSpeed = 2f;
+
+    [Range(-180f, 180f)] public float rotationX, rotationY, rotationZ;
+    void Update()
+    {
+        transform.Rotate(rotationX * (rotationSpeed * Time.deltaTime), rotationY * (rotationSpeed * Time.deltaTime),rotationZ * (rotationSpeed * Time.deltaTime), Space.Self);
+    }
+}
